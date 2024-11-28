be6c9b9d766f7e555ff7afc1a0896802ebc068d9
pertodomato <134084297+pertodomato@users.noreply.github.com>
2024-11-28 16:58:28 -0300

    Update settings.py

--- polisalas/settings.py
+++ polisalas/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = False
-ALLOWED_HOSTS = ['Polisalas.onrender.com']
+ALLOWED_HOSTS = ['polisalas.onrender.com']
 
 ALLOWED_HOSTS = []
 
